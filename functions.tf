cd8c01690b47bfc1d517ed859daf672212727232
eduardobrl <eduardobrl10@gmail.com>
2024-06-13 17:51:18 -0300

    add initial files for module

---
+++ functions.tf
@@ -0,0 +1,67 @@
+resource "aws_bedrockagent_agent_action_group" "scheduler_assistant" {
+  for_each = {
+    for index, function in var.functions:
+    function.name => function 
+  }
+
+  action_group_name           = "${each.value.name}_action_group"
+  agent_id                    = aws_bedrockagent_agent.scheduler_assistant.agent_id
+  agent_version               = "DRAFT"
+  skip_resource_in_use_check  = true
+
+  action_group_executor {
+    lambda = "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${each.value.name}"
+  }
+
+  api_schema {
+    payload = file(each.value.schema)
+  }
+  
+  
+}
+
+
+
+resource "aws_lambda_function" "lambda" {
+  for_each = {
+    for index, function in var.functions:
+    function.name => function 
+  }
+  filename         = each.value.filename
+  function_name    = each.value.name
+  role             = aws_iam_role.lambda_role[each.key].arn
+  handler          = each.value.handler
+  runtime          = each.value.runtime
+}
+
+resource "aws_iam_role" "lambda_role" {
+  for_each = {
+    for index, function in var.functions:
+    function.name => function 
+  }
+  name = "${each.value.name}-role"
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  EOF
+  
+}
+
+resource "aws_iam_role_policy_attachment" "bedrock_query_lambda_basic_execution" {
+  for_each = {
+    for index, function in var.functions:
+    function.name => function 
+  }
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       =  "${each.value.name}-role"
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "foundation_model" {
+  description = "The name foundation model to use for the Bedrock agent"
+  type        = string
+  default     = "claude-v1"
+}
+
+variable "instructions" {
+  description = "The instructions for bedrock agent"
+  type        = string
+  default     = "claude-v1"
+}
+
+variable "app_name" {
+  description = "The name of ther service"
+  type        = string
+}
+
+variable "functions" {
+  description = "Lambda functions to handle bedrock tools"
+  type        = set(object({
+    name     = string
+    handler  = string
+    runtime  = string
+    filename = string
+    schema   = string
+  }))
+}
